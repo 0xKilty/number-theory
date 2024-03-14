4e80538076426b376b6f2081ac55ba28e98c4a7a
0xKilty <iankilty1@gmail.com>
2024-03-13 19:39:55 -0600

    progress on html gen

--- scripts/generate_html.py
+++ scripts/generate_html.py
@@ -1,9 +1,30 @@
 import git
+import re
 
-repo = git.Repo("..")
-main_branch = repo.commit("main")
-tree = main_branch.tree
+def get_functions_data_from_file(file):
+    pattern = r'(?P<name>\b(?:int|void|char|float|double)\s+(\w+)\s*\([^)]*\)\s*{)'
+    matches = re.finditer(pattern, file)
+    function_info = []
+    for match in matches:
+        function_name = match.group('name')[:-2]
+        start_line = file.count('\n', 0, match.start()) + 1
+        function_info.append((function_name, start_line))
+    return function_info
 
-for blob in tree.traverse():
-    if blob.type == 'blob':
-        print(blob.path)
+def main():
+    repo = git.Repo("..")
+    default_branch = "main"
+
+    main_branch = repo.commit(default_branch)
+    tree = main_branch.tree
+
+    for blob in tree.traverse():
+        if blob.type == 'blob':
+            if blob.path.split(".")[-1] == "c":
+                file = repo.git.show(f"{default_branch}:{blob.path}")
+                function_info = get_functions_data_from_file(file)
+                for i in function_info:
+                    print(i)
+
+if __name__ == "__main__":
+    main()
