3fb52f05ecdd773f1b27eac98feb788e3c28056f
0xKilty <iankilty1@gmail.com>
2024-03-18 09:52:14 -0600

    try it now

--- scripts/host.py
+++ scripts/host.py
@@ -2,32 +2,9 @@ import http.server
 import socketserver
 import os
 
-class NoCacheHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
-    def send_head(self):
-        path = self.translate_path(self.path)
-        f = None
-        if os.path.isdir(path):
-            return self.list_directory(path)
-        try:
-            f = open(path, 'rb')
-        except OSError:
-            self.send_error(404, "File not found")
-            return None
-        try:
-            self.send_response(200)
-            self.send_header("Content-type", self.guess_type(path))
-            self.send_header("Cache-Control", "no-store, must-revalidate")
-            self.send_header("Pragma", "no-cache")
-            self.send_header("Expires", "0")
-            self.end_headers()
-            return f
-        except:
-            f.close()
-            raise
+PORT = 8002
 
-PORT = 8001
-
-Handler = NoCacheHTTPRequestHandler
+Handler = http.server.SimpleHTTPRequestHandler
 os.chdir("../docs")
 
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
