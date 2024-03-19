b9de80d06b12f3487e7418a2972039eaa38d1daa
0xKilty <iankilty1@gmail.com>
2024-03-19 08:14:28 -0600

    wat

--- python/common_functions.py
+++ python/common_functions.py
@@ -2,3 +2,12 @@ def gcd(a: int, b: int) -> int:
     while b != 0:
         a, b = b, a % b
     return a
+
+def extended_gcd(a, b):
+    x_prev, x, y_prev, y = 0, 1, 1, 0
+    while b != 0:
+        q = a // b
+        a, b = b, a % b
+        x_prev, x = x - q * x_prev, x_prev
+        y_prev, y = y - q * y_prev, y_prev
+    return a, x, y
