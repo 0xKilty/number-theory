f2454382fce11d8209034fa51aa9c8418325e80e
0xKilty <iankilty1@gmail.com>
2024-03-25 17:28:57 -0600

    wat

--- python/setup.py
+++ python/setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='numtheory',
+    name='numbertheory',
     version='0.1',
-    packages=find_packages(),
     description='Common number theoretic functions.',
-    license=' GPL-3.0 license',
+    packages=find_packages(where='number-theory'),
+    package_dir={'': 'number-theory'},
+    license='GPL-3.0 license',
     url='https://0xkilty.github.io/number-theory/',
     author='0xKilty',
     author_email='ian.kilty@colostate.edu',
