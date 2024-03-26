fa166f6443c4072ca46653e5c6ab10e11b7c8212
0xKilty <iankilty1@gmail.com>
2024-03-25 18:58:07 -0600

    wat

---
+++ python/numbertheory/__init__.py
@@ -0,0 +1,6 @@
+from .common_functions import *
+from .fibonacci import *
+from .generating_primes import *
+from .integer_factorization import *
+from .modulus_operations import *
+from .primality_tests import *
--- python/setup.py
+++ python/setup.py
@@ -2,10 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name='numbertheory',
-    version='0.1',
+    version='0.0.1',
     description='Common number theoretic functions.',
-    packages=find_packages(where='number-theory'),
-    package_dir={'': 'number-theory'},
+    packages=find_packages(),
     license='GPL-3.0 license',
     url='https://0xkilty.github.io/number-theory/',
     author='0xKilty',
