441c592c081b5b62cd965860d21c35ae0306cb54
0xKilty <iankilty1@gmail.com>
2024-03-25 20:45:14 -0600

    wat

--- benchmarking/scripts/generate_csv.py
+++ benchmarking/scripts/generate_csv.py
@@ -8,7 +8,6 @@ num_iterations = 10000
 
 random_numbers_list = [generate_random_numbers() for _ in range(num_iterations)]
 
-with open('random_numbers.csv', 'w', newline='') as csvfile:
+with open('../data/random_numbers.csv', 'w', newline='') as csvfile:
     writer = csv.writer(csvfile)
-    writer.writerow(['First Number', 'Second Number'])  # Writing header
     writer.writerows(random_numbers_list)
