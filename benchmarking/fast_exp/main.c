1ca1e2e0e8e3ad76e97b5261c89327d544a06cb3
0xKilty <iankilty1@gmail.com>
2024-03-27 09:19:34 -0600

    wat

--- benchmarking/fast_exp/main.c
+++ benchmarking/fast_exp/main.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 #define MAX_LINE_LENGTH 1024
 #define MAX_COLUMNS 2
+
+struct Pair {
+    long long a;
+    long long b;
+};
+
+long long regular_exp(long long a, long long b) {
+    long long res = 1;
+    for (int i = 0; i < a; i++) {
+        res *= a;
+    }
+    return res;
+}
 
 long long fast_exp(long long a, long long b) {
     long long res = 1;
@@ -16,30 +31,52 @@ long long fast_exp(long long a, long long b) {
     return res;
 }
 
-int main() {
-    printf("%lld\n", fast_exp(31415, 69));
-    
-    char filename[] = "../data/random_numbers.csv";
-    FILE *file = fopen(filename, "r");
+int get_number_of_rows(FILE *file) {
+    char line[MAX_LINE_LENGTH];
+    int num_rows = 0;
+    while (fgets(line, MAX_LINE_LENGTH, file) != NULL) {
+        num_rows++;
+    }
+    rewind(file);
+    return num_rows;
+}
+
+void read_csv_file(FILE *file, struct Pair *pairs) {
+    char line[MAX_LINE_LENGTH];
+    int index = 0;
+    while (fgets(line, MAX_LINE_LENGTH, file) != NULL) {
+        long long first, second;
+        if (sscanf(line, "%lld,%lld", &first, &second) != 2) {
+            fprintf(stderr, "Error parsing line: %s\n", line);
+            continue;
+        }
+
+        pairs[index].a = first;
+        pairs[index].b = second;
+        index++;
+    }
+}
+
+int main() {    
+    FILE *file = fopen("../data/random_numbers.csv", "r");
     if (file == NULL) {
-        fprintf(stderr, "Could not open file %s\n", filename);
+        perror("Error opening file");
         return 1;
     }
 
-    char line[MAX_LINE_LENGTH];
-    while (fgets(line, sizeof(line), file)) {
-        line[strcspn(line, "\n")] = '\0';
+    int num_rows = get_number_of_rows(file);
+    struct Pair *pairs = (struct Pair *)malloc(num_rows * sizeof(struct Pair));
+    read_csv_file(file, pairs);
 
-        char *token;
-        char *rest = line;
-        int column_count = 0;
-        while ((token = strtok_r(rest, ",", &rest)) != NULL) {
-            printf("Value: %s\n", token);
-            column_count++;
-            if (column_count >= MAX_COLUMNS)
-                break;
-        }
+    clock_t start = clock();
+    for (int i = 0; i < num_rows; i++) {
+        printf("%lld ^ %lld = %lld\n", pairs[i].a, pairs[i].b, fast_exp(pairs[i].a, pairs[i].b));
     }
+    clock_t end = clock();
+    double cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+    printf("Time taken: %f seconds\n", cpu_time_used);
+
+    free(pairs);
 
     fclose(file);
 
