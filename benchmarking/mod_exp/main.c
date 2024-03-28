d90eece3c8d53232c7767dfb42cfbd7b7fa2acf6
0xKilty <iankilty1@gmail.com>
2024-03-27 19:02:43 -0600

    wat

---
+++ benchmarking/mod_exp/main.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <gmp.h>
+
+#define MAX_LINE_LENGTH 1024
+#define MAX_COLUMNS 2
+
+struct Pair {
+    long long a;
+    long long b;
+};
+
+long long regular_exp(long long a, long long b) {
+    long long res = 1;
+    for (int i = 0; i < a; i++) {
+        res *= a;
+    }
+    return res;
+}
+
+long long fast_exp(long long a, long long b) {
+    long long res = 1;
+    while (b > 0) {
+        if (b & 1) {
+            res *= a;
+        }
+        a *= a;
+        b >>= 1;
+    }
+    return res;
+}
+
+int get_number_of_rows(FILE *file) {
+    char line[MAX_LINE_LENGTH];
+    int num_rows = 0;
+    while (fgets(line, MAX_LINE_LENGTH, file) != NULL) {
+        num_rows++;
+    }
+    rewind(file);
+    return num_rows;
+}
+
+void read_csv_file(FILE *file, struct Pair *pairs) {
+    char line[MAX_LINE_LENGTH];
+    int index = 0;
+    while (fgets(line, MAX_LINE_LENGTH, file) != NULL) {
+        long long first, second;
+        if (sscanf(line, "%lld,%lld", &first, &second) != 2) {
+            fprintf(stderr, "Error parsing line: %s\n", line);
+            continue;
+        }
+
+        pairs[index].a = first;
+        pairs[index].b = second;
+        index++;
+    }
+}
+
+int main() {    
+    FILE *file = fopen("../data/random_numbers.csv", "r");
+    if (file == NULL) {
+        perror("Error opening file");
+        return 1;
+    }
+
+    int num_rows = get_number_of_rows(file);
+    struct Pair *pairs = (struct Pair *)malloc(num_rows * sizeof(struct Pair));
+    read_csv_file(file, pairs);
+
+    mpz_t base, exponent, result;
+    mpz_inits(base, exponent, result, NULL);
+
+    clock_t start = clock();
+    for (int i = 0; i < num_rows; i++) {
+        mpz_set_si(base, pairs[i].a);
+        mpz_set_si(exponent, pairs[i].b);
+        mpz_pow_ui(result, base, mpz_get_ui(exponent));
+        //gmp_printf("Result: %Zd\n", result);
+    }
+    clock_t end = clock();
+    double cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+    printf("Time taken: %f seconds\n", cpu_time_used);
+
+    free(pairs);
+    mpz_clears(base, exponent, result, NULL);
+
+    fclose(file);
+
+    return 0;
+}
